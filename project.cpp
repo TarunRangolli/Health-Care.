762874fc261916442df55e2ce9dd4de2aba89c08
TarunRangolli <02fe23bcs042@kletech.ac.in>
2025-01-21 08:51:03 +0530

    Create project.cpp

---
+++ project.cpp
@@ -0,0 +1,48 @@
+
+Welcome to My Project
+Patient Symptom Analysis and Self diagnosis 
+
+Patient Symptom Analysis and Self diagnosis Project
+Project: Patient Symptom Analysis and Self Diagnosis
+
+1st Module: Ambiguous Symptom Entry Patients describe their symptoms using natural language, where the system processes partial or unclear inputs to identify possible symptoms using advanced matching algorithms (e.g., levenstindistance or KMP).
+
+2nd Module: Limited Medical Awareness Based on the identified symptoms, the system asks targeted yes/no questions to narrow down possibilities and predict the most likely disease using decision analysis techniques.(Desicion tree)
+
+3rd Module: Shortest Path to Medical Facility Once a disease is identified, the system calculates the shortest path to the nearest medical facility using graph-based algorithms like Dijkstra's for quick access to care.(Steps of Dijkstra’s Algorithm)
+
+4th Module: Prescription and Precaution After confirming the disease, the system provides a tailored prescription, lifestyle precautions, and advice, ensuring the patient takes immediate steps for recovery.(Brute force string matching )
+
+#.github.io
+
+Portfolio
+Hello, Praveen Palkar 👋
+I am passionate about technology and problem-solving. Here's a glimpse of my work:
+
+🛠 Projects
+Project 1: The Patient Symptom Analysis and Self Diagnosis system is a comprehensive tool designed to enhance the healthcare experience for individuals. It intelligently processes vague or unclear symptom inputs, narrows down diagnoses through targeted questions, ensures quick access to medical facilities, and provides personalized treatment recommendations. With these four integrated modules, the system aims to provide timely and accurate healthcare guidance, empowering patients to make informed decisions while seeking medical attention.
+##view code
+
+You can view the complete code for the project here : View my code
+
+🚀 Skills
+C++, Python, C
+Data Structures and Algorithms
+🌐 Find Me Online
+GitHub
+Portfolio Topic
+Course Name
+Algorithmic Lab
+Course Code
+24ECSP205
+Name
+Tarun Rangolli
+SRN
+02FE23BCS042
+Course Instructor
+Prof.Vaishali Parab
+University
+KLE Technological University
+Patient Symptom Analysis and Self Diagnosis/COMPUTER SCIENCE
+
+> “The only way to do great work is to love what you do.” – Steve Jobs >
